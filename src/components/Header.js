bae89d169f5168d6ed2d78bef073d20d5327d6c9
Jan Muhachi <jan.muhachi@gmail.com>
2024-04-23 12:10:51 +0300

    Done lab

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,9 @@
 import React from "react";
 
 function Header(props) {
-      return (
-        <h1>
-          {/* uncomment the line below to pass the test */}
-          {/* hello from the Header! */}
-        </h1>
-      )
+      return <return>hello from the Header!</return>
+        
+         
 }
 
 export default Header;
